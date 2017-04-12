3a6a6a5f69ed07b668cfd167027a96e11c6bc092
Clark124 <53799906@qq.com>
2017-04-12 14:27:00 +0800

    remove all files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <h1>hello Clark</h1>,
-  document.getElementById('root')
-);
